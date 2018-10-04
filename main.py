18c0e53cd180c061b96c064a6b5157c49b9bf827
Carl Yarwood <cdy8858@vh222006.truman.edu>
2018-10-04 18:34:03 -0500

    added code to help interpret orfs

--- main.py
+++ main.py
@@ -3,13 +3,41 @@ from translate import translate
 from translate import inverseTransverse
 from codon_processing import process_dna_strand
 from codon_processing import PossibleReadingFrame
-from codon_processing import deleteDuplicates
+from orf_interpretation import removeOptionsOfWrongSize
+from orf_interpretation import getPromoterValue
+
+class ORF:
+    def __init__(self, possibleReadingFrame, promoterValue, sequence):
+        self.possibleReadingFrame = possibleReadingFrame
+        self.promoterValue = promoterValue
+        self.sequence = sequence
+
 if(len(argv) != 2):
     print("Usage:  python3 main.py <fasta filename>.fasta")
 else:
     dna1 = translate(argv[1])
     dna2 = inverseTransverse(dna1)
     possibleReadingFrameArr1 = process_dna_strand(dna1)
-    possibleReadingFrameArr1 = deleteDuplicates(possibleReadingFrameArr1)
     possibleReadingFrameArr2 = process_dna_strand(dna2)
-    possibleReadingFrameArr2 = deleteDuplicates(possibleReadingFrameArr2)
+    possibleReadingFrameArr1 = removeOptionsOfWrongSize(possibleReadingFrameArr1)
+    possibleReadingFrameArr2 = removeOptionsOfWrongSize(possibleReadingFrameArr2)
+    orfWithValue1 = []
+    orfWithValue2 = []
+    for elements in possibleReadingFrameArr1:
+        orfWithValue1.append(ORF(element,getPromoterValue(dna1[max(0,element.startIndex - 200):max(0, elment.startIndex -50 + 1)]),dna1[element.startIndex:element.stopIndex+3] ))
+    for elements in possibleReadingFrameArr2:
+        orfWithValue2.append(ORF(element,getPromoterValue(dna2[max(0,element.startIndex - 200):max(0, elment.startIndex -50 + 1)]),dna2[element.startIndex:element.stopIndex+3] ))
+    final2 = []
+    final2.append(getBestOrf(orfWithValue1))
+    final2.append(getBestOrf(orfWithValue2))
+    bestOrf = getBestOrf(final2)
+    print(bestOrf.sequence)
+    
+    
+
+def getBestOrf(orfWithValueArray):
+    max = orfWithValueArray[0]
+    for element in orfWithValueArray:
+        if element.promoterValue > max.promoterValue :
+            max = element
+    return max
--- orf_interpretation.py
+++ orf_interpretation.py
@@ -1,1 +1,34 @@
 #orf orf orf orf orf
+#TATA(A/T)A(A/T)(A/G)
+import numpy
+from codon_processing import PossibleReadingFrame
+
+
+promoter = ["TATAAAAA","TATATAAA","TATAAATA","TATAAAAG","TATATATA","TATATAAG","TATAAATG","TATATATG"]
+gapPenatly = -2
+matchPenalty = -1
+matchReward = 1
+def getPromoterValue(seq):
+    possibleValueArr = []
+    for element in promote:
+        possibleValueArr.append(fuzzyMatch(element, sequence))
+    return max(possibleValueArr)
+
+def fuzzyMatch(str1, str2):
+    workstr1 = "-" + str1
+    workstr2 = "-" + str2
+    matchArr = numpy.zeros(len(workstr1), len(workstr2))
+    for index in range(len(workstr1)):
+        matchArr[index][0] = index * gapPenalty
+    for row in range(len(workstr1)-1):
+        for col in range(len(workstr2) -1):
+            possibleChoices = [(matchArr[row][col+1] + gapPenalty), (matchArr[row+1][col] + gapPenalty), ((matchArr[row][col] + matchReward) if (str1(row + 1) == str(col + 1)) else (matchArr[row][col] + matchPenalty))]
+            matchArr[row+1][col+1] = getMax(possibleChoices)
+    print( matchArr)
+    return max(matchArr[len(workstr1) - 1])
+
+def removeOptionsOfWrongSize(possibleReadingFrameArr):
+    for element in reverse(possibleReadingFrameArr):
+        if(element.stopIndex + 2 - element.startIndex < 300):
+            possibleReadingFrame.remove(element)
+    return possibleReadingFrame
