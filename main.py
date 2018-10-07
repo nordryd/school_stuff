4a07c0b23bfee7bccdbbeada60f9cad75eecf16b
Carl Yarwood <cdy8858@vh222006.truman.edu>
2018-10-06 22:27:22 -0500

    final functionality added

--- main.py
+++ main.py
@@ -15,34 +15,68 @@ class ORF:
         self.frame = frame
 
 def getBestOrf(orfWithValueArray):
-    maxElement = orfWithValueArray[0]
+    maxElement = []
+    bestValue = -57 #note the lowest match value is -56 as all our promoters are 8 base pairs long
     for element in orfWithValueArray:
-        if element.promoterValue > maxElement.promoterValue :
-            maxElement = element
+        if element.promoterValue > bestValue :
+            bestValue = element.promoterValue
+            maxElement = []
+            maxElement.append(element)
+        elif element.promoterValue == maxElement[0].promoterValue:
+            maxElement.append(element)
     return maxElement
-if(len(argv) != 2):
-    print("Usage:  python3 main.py <fasta filename>.fasta")
-else:
-    dna1 = translate(argv[1])
-    dna2 = inverseTransverse(dna1)
-    possibleReadingFrameArr1 = process_dna_strand(dna1)
-    possibleReadingFrameArr2 = process_dna_strand(dna2)
-    possibleReadingFrameArr1 = removeOptionsOfWrongSize(possibleReadingFrameArr1)
-    possibleReadingFrameArr2 = removeOptionsOfWrongSize(possibleReadingFrameArr2)
-
-    orfWithValue1 = []
-    orfWithValue2 = []
-    for elements in possibleReadingFrameArr1:
-        orfWithValue1.append(ORF(elements,getPromoterValue(dna1[max(0,elements.startIndex - 200):max(0, elments.startIndex -50 + 1)]),dna1[elements.startIndex:elements.stopIndex+3], 1, elements.startIndex%3 + 1 ))
-    for elements in possibleReadingFrameArr2:
-        orfWithValue2.append(ORF(elements,getPromoterValue(dna2[max(0,elements.startIndex - 200):max(0, elements.startIndex -50 + 1)]),dna2[elements.startIndex:elements.stopIndex+3], 2, elements.startIndex%3 +1 ))
-    final2 = []
-    if(len(orfWithValue1) > 0):
-        final2.append(getBestOrf(orfWithValue1))
-    if(len(orfWithValue2) > 0):
-        final2.append(getBestOrf(orfWithValue2))
-    if(len(final2) > 0):
-        bestOrf = getBestOrf(final2)
-        print(bestOrf.dnaStrand, bestOrf.frame,bestOrf.possibleReadingFrame.startIndex, bestOrf.possibleReadingFrame.stopIndex,  bestOrf.sequence)
+def main(fileName, fileNumber):
+    if(False): #replace with len(argv) != 2 when done testing
+        print("Usage:  python3 main.py <fasta filename>.fasta")
     else:
-        print("no valid sequence was found")
+        dna1 = translate(fileName) #replace with argv[1] when done testing
+        dna2 = inverseTransverse(dna1)
+        possibleReadingFrameArr1 = process_dna_strand(dna1)
+        possibleReadingFrameArr2 = process_dna_strand(dna2)
+        possibleReadingFrameArr1 = removeOptionsOfWrongSize(possibleReadingFrameArr1)
+        possibleReadingFrameArr2 = removeOptionsOfWrongSize(possibleReadingFrameArr2)
+
+        orfWithValue1 = []
+        orfWithValue2 = []
+        for elements in possibleReadingFrameArr1:
+            orfWithValue1.append(ORF(elements, getPromoterValue(dna1[max(0,elements.startIndex - 200):max(0, elements.startIndex -50 + 1)]),dna1[elements.startIndex:elements.stopIndex+3], 1, elements.startIndex%3 + 1 ))
+        for elements in possibleReadingFrameArr2:
+            orfWithValue2.append(ORF(elements,getPromoterValue(dna2[max(0,elements.startIndex - 200):max(0, elements.startIndex -50 + 1)]),dna2[elements.startIndex:elements.stopIndex+3], 2, elements.startIndex%3 +1 ))
+        finals = []
+        if(len(orfWithValue1) > 0):
+            bestORF1 = getBestOrf(orfWithValue1)
+            for element in bestORF1:
+                finals.append(element)
+        if(len(orfWithValue2) > 0):
+            bestORF2 = getBestOrf(orfWithValue2)
+            for element in bestORF2:
+                finals.append(element)
+        if(len(finals) > 0):
+            bestOrf = getBestOrf(finals)
+            #print(bestOrf.dnaStrand, bestOrf.frame,bestOrf.possibleReadingFrame.startIndex, bestOrf.possibleReadingFrame.stopIndex,  bestOrf.sequence)
+            frame = [0,0,0,0,0,0]
+            for element in bestOrf :
+                if element.possibleReadingFrame.startIndex % 3 == 0 and element.dnaStrand == 1 :
+                    frame[0] = frame[0] + 1
+                elif element.possibleReadingFrame.startIndex %3 == 1 and element.dnaStrand == 1:
+                    frame[1] = frame[1] + 1
+                elif element.possibleReadingFrame.startIndex %3 == 2 and element.dnaStrand == 1:
+                    frame[2] = frame[2] + 1
+                elif element.possibleReadingFrame.startIndex %3 == 0 and element.dnaStrand == 2:
+                    frame[3] = frame[3] + 1
+                elif element.possibleReadingFrame.startIndex %3 == 1 and element.dnaStrand == 2:
+                    frame[4] = frame[4] + 1
+                else:
+                    frame[5] = frame[5] + 1
+            bestframe = 0
+
+            for index in range(6):
+                if frame[index] > frame[bestframe]:
+                    bestframe = index
+            bestframe = bestframe +1
+            print(bestframe)
+                
+                #print(fileNumber, element.dnaStrand, element.possibleReadingFrame.startIndex, element.possibleReadingFrame.stopIndex + 3, len(dna1) - 2000)
+        else:
+            print("no valid sequence was found")
+#main()
