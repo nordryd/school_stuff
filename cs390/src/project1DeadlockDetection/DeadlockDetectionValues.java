f3ff89df1e4dd4214fc7d4660e3a6427a11cda3b
Nordryd <nordryd13@gmail.com>
2018-10-15 16:19:04 -0500

    Fixed USAGE_STRING and generated new JAR file

--- cs390/src/project1DeadlockDetection/DeadlockDetectionValues.java
+++ cs390/src/project1DeadlockDetection/DeadlockDetectionValues.java
@@ -1,7 +1,7 @@
 package project1DeadlockDetection;
 
 public class DeadlockDetectionValues {
-	public static final String USAGE_STRING = "Usage: java -jar DeadlockDetection.jar <input file name>\nFile must be formatted:\n\n"
+	public static final String USAGE_STRING = "Usage: java -jar DeadlockDetection.jar <input file name>\n\nFile must be formatted:\n"
 			+ "Line 1: Number of processes N, N > 0\n" + "Line 2: Number of resource types M, M > 0\n"
 			+ "Line 3: Contents of the array Avail, separated by a whitespace\n"
 			+ "Next N Lines: Contents of the array Alloc, separated by a whitespace.\n"
